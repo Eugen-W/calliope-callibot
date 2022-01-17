e84f85bb54b7823049fe5fe272a5b5bbdba4af2e
Eugen Wenner <97448464+Eugen-W@users.noreply.github.com>
2022-01-17 15:31:44 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
-input.onButtonPressed(Button.A, function () {
-    callibot.warteLSensor(KSensor.links, KSensorStatus.dunkel)
+basic.forever(function () {
+    if (callibot.readLineSensor(KSensor.links, KSensorStatus.dunkel)) {
+        callibot.setLed(KMotor.links, KState.an)
+        callibot.motor(KMotor.rechts, KDir.vorwärts, 100)
+    } else {
+        callibot.setLed(KMotor.links, KState.aus)
+        callibot.motorStop(KMotor.rechts, KStop.Frei)
+    }
 })
 basic.forever(function () {
-	
+    if (callibot.readLineSensor(KSensor.rechts, KSensorStatus.dunkel)) {
+        callibot.setLed(KMotor.rechts, KState.an)
+        callibot.motor(KMotor.links, KDir.vorwärts, 100)
+    } else {
+        callibot.setLed(KMotor.rechts, KState.aus)
+        callibot.motorStop(KMotor.links, KStop.Frei)
+    }
 })
