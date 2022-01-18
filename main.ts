eab0ff80348ad9cb3446baf741547427c3a56e05
dran3 <97412804+dran3@users.noreply.github.com>
2022-01-18 10:42:01 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    callibot.motor(KMotor.beide, KDir.vorwarts, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    callibot.motorStop(KMotor.beide, KStop.Bremsen)
+})
 basic.forever(function () {
     if (callibot.readLineSensor(KSensor.links, KSensorStatus.dunkel)) {
         callibot.setLed(KMotor.links, KState.an)
-        callibot.motor(KMotor.rechts, KDir.vorwärts, 100)
+        callibot.motor(KMotor.rechts, KDir.vorwarts, 100)
     } else {
         callibot.setLed(KMotor.links, KState.aus)
         callibot.motorStop(KMotor.rechts, KStop.Frei)
@@ -10,7 +16,7 @@ basic.forever(function () {
 basic.forever(function () {
     if (callibot.readLineSensor(KSensor.rechts, KSensorStatus.dunkel)) {
         callibot.setLed(KMotor.rechts, KState.an)
-        callibot.motor(KMotor.links, KDir.vorwärts, 100)
+        callibot.motor(KMotor.links, KDir.vorwarts, 100)
     } else {
         callibot.setLed(KMotor.rechts, KState.aus)
         callibot.motorStop(KMotor.links, KStop.Frei)
