63736aafb0ea3f43b088d1f112b5614c7dc9fe1f
dran3 <97412804+dran3@users.noreply.github.com>
2022-01-20 11:42:49 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+function drehen () {
+    drehenTimer += -1
+    callibot.motor(KMotor.rechts, KDir.rückwärts, 50)
+    callibot.motor(KMotor.links, KDir.vorwärts, 50)
+    if (drehenTimer == 0) {
+        status = 0
+        kreisTimer = 75
+    }
+}
 function sucheKreis () {
     if (callibot.readLineSensor(KSensor.links, KSensorStatus.dunkel) && callibot.readLineSensor(KSensor.rechts, KSensorStatus.dunkel)) {
         kreisTimer += -1
@@ -5,18 +14,23 @@ function sucheKreis () {
         callibot.motorStop(KMotor.links, KStop.Bremsen)
     } else if (callibot.readLineSensor(KSensor.links, KSensorStatus.dunkel) && callibot.readLineSensor(KSensor.rechts, KSensorStatus.hell)) {
         callibot.motor(KMotor.beide, KDir.rückwärts, 25)
+    } else if (callibot.readLineSensor(KSensor.links, KSensorStatus.hell) && callibot.readLineSensor(KSensor.rechts, KSensorStatus.dunkel)) {
+        kreisTimer += -1
+        callibot.motor(KMotor.links, KDir.vorwärts, 25)
+        callibot.motorStop(KMotor.rechts, KStop.Bremsen)
     } else {
         callibot.motor(KMotor.beide, KDir.vorwärts, 25)
     }
     if (kreisTimer == 0) {
-        kreisgefunden = 1
+        status = 1
     }
 }
 input.onButtonPressed(Button.A, function () {
     fahren = 1
-    Zeit = 500
-    kreisgefunden = 0
+    Zeit = 1000
+    status = 0
     kreisTimer = 75
+    drehenTimer = 35
 })
 function fahrenimkreis () {
     if (callibot.readLineSensor(KSensor.links, KSensorStatus.dunkel) && callibot.readLineSensor(KSensor.rechts, KSensorStatus.dunkel)) {
@@ -36,33 +50,31 @@ function fahrenimkreis () {
         callibot.motorStop(KMotor.beide, KStop.Frei)
         callibot.setLed(KMotor.beide, KState.aus)
     }
-    if (callibot.entfernung(KEinheit.cm) < 20) {
-        fahren = 0
-        GegenstandImWeg = 1
-    }
 }
 input.onButtonPressed(Button.B, function () {
     fahren = 0
 })
 let Zeit = 0
 let fahren = 0
-let kreisgefunden = 0
 let kreisTimer = 0
+let status = 0
+let drehenTimer = 0
 let GegenstandImWeg = 0
-GegenstandImWeg = 0
 basic.forever(function () {
     Zeit += -1
     if (Zeit > 0 && fahren == 1) {
-        if (kreisgefunden == 1) {
+        if (status == 1) {
             fahrenimkreis()
-        } else if (kreisgefunden == 0) {
+        } else if (status == 0) {
             sucheKreis()
+        } else if (status == 2) {
+            drehen()
         }
     } else {
         callibot.motorStop(KMotor.beide, KStop.Frei)
         callibot.setLed(KMotor.beide, KState.aus)
     }
-    if (GegenstandImWeg == 1 && callibot.entfernung(KEinheit.cm) > 20) {
-        fahren = 1
+    if (callibot.entfernung(KEinheit.cm) < 15) {
+        status = 2
     }
 })
